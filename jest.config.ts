b0e03f6578caf3df4eb8b972797e342336472746
SheikhJack <kealebogakido@gmail.com>
2024-10-30 13:51:17 +0200

    test2

--- jest.config.ts
+++ jest.config.ts
@@ -2,7 +2,7 @@ import type {Config} from 'jest';
 import {defaults} from 'jest-config';
 
 const config: Config = {
-  moduleFileExtensions: [...defaults.moduleFileExtensions, 'mts'],
+  moduleFileExtensions: [...defaults.moduleFileExtensions || [], 'mts'],
   testEnvironment: 'jest-environment-jsdom',
 };
 
