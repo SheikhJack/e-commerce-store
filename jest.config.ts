824ad40907d2af42899893699e94390a4db5ebeb
SheikhJack <kealebogakido@gmail.com>
2024-10-30 13:07:47 +0200

    jest

---
+++ jest.config.ts
@@ -0,0 +1,10 @@
+import type {Config} from 'jest';
+import {defaults} from 'jest-config';
+
+const config: Config = {
+  moduleFileExtensions: [...defaults.moduleFileExtensions, 'mts'],
+  testEnvironment: 'jest-environment-jsdom',
+};
+
+export default config;
+  
